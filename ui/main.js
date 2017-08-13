b30e3963acd9228b954338ec4965b05b9bf00ca1
saravanakj <saravanakj@gmail.com>
2017-08-13 08:45:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,12 @@ mainTxt.innerHTML = 'Hi! Modified...';
 
 var img = document.getElementById('madi');
 
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+
 img.onclick = function(){
-    img.style.marginLeft = '100px';
+    setInterval(moveRight, 100);
 };
