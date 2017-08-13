1bf44f2dcb07145a260806ea0834595245a60f3f
saravanakj <saravanakj@gmail.com>
2017-08-13 08:41:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,6 @@ console.log('Loaded!');
 
 var mainTxt = document.getElementById('main-txt');
 mainTxt.innerHTML = 'Hi! Modified...';
+
+var img = document.getElementById('madi');
+img.style.marginLeft = '100px';
