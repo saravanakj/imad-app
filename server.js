a66dd9c3864ba8a131cae2e987b1d0b792da75db
saravanakj <saravanakj@gmail.com>
2017-08-14 17:16:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.get('/counter', function (req, res) {
 
 var names=[];
 app.get('/subname', function(req, res){
-   var name = req.params.name;
+   var name = req.query.name;
    names.push(name);
    res.send(JSON.stringify(names));
 });
