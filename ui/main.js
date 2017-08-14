6e3ce5e879c770b23f11968de6b3df8988db51f3
saravanakj <saravanakj@gmail.com>
2017-08-14 17:22:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -59,7 +59,7 @@ submitBtn.onclick = function(){
       }  
     };
     
-    request.open('GET', 'http://saravanakj.imad.hasura-app.io/subname='+document.getElementById('nameTxt').text, true);
+    request.open('GET', 'http://saravanakj.imad.hasura-app.io/subname?name='+document.getElementById('nameTxt').text, true);
     request.send(null);
     
     
