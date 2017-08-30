0cfe45594c6798d7e03a41be8f412057a6471020
Saravana Kumar J <saravanakj@gmail.com>
2017-08-30 08:22:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+var config = {
+    host: 'localhost:5432',
+    user: 'saravanakj',
+    database: 'saravanakj',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -64,6 +71,17 @@ var createTemplate = function(data){
     return template;
     
 }
+
+var pool = new Pool(config);
+app.get('/test-db', function(req, res) {
+    pool.query('select * from user', function(err, result){
+       if(err){
+           res.status(500).send(err.toString());
+       } else {
+           res.send(JSON.stringify(result));
+       }
+    });
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
