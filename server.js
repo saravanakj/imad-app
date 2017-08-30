c3b80afd9592a73a517a0cfb033f2c0fbf0ba86f
Saravana Kumar J <saravanakj@gmail.com>
2017-08-30 08:43:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,7 +111,11 @@ app.get('/articles/:articleName', function (req, res) {
        if(err){
            res.status(500).send(err.toString());
        } else {
-           res.send(createTemplate(result.rows[0]));
+           if(result.rows.length === 0) {
+               res.status(404).send('Article not found');
+           } else {
+            res.send(createTemplate(result.rows[0]));
+           }
        }
   });
 });
