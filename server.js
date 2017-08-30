992ae5ba77cf39a4924cfea39ba0fb7a9f0ff80d
Saravana Kumar J <saravanakj@gmail.com>
2017-08-30 08:25:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ var createTemplate = function(data){
 
 var pool = new Pool(config);
 app.get('/test-db', function(req, res) {
-    pool.query('select * from user', function(err, result){
+    pool.query('select * from "user"', function(err, result){
        if(err){
            res.status(500).send(err.toString());
        } else {
