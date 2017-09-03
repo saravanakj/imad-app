fb25cd92eb82ad5b987613a99767ab47905dbb5e
Saravana Kumar J <saravanakj@gmail.com>
2017-09-03 16:18:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -74,11 +74,16 @@ loginBtn.onclick = function(){
       if(request.readyState === XMLHttpRequest.DONE){
           if(request.status === 200){
                alert('user logged in..!')
-          }
+          } else if (request.status === 403) {
+              alert('invalid credentials');
+          } else if (request.status === 500) {
+              alert('server error');
+          } 
       }  
     };
     
     request.open('POST', 'http://saravanakj.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify({
         "userName": document.getElementById('userTxt').value,
         "password": document.getElementById('pwdTxt').value
